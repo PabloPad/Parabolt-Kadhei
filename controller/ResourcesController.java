bc97830006b006866ba90d5fe0b3a863d423f886
Pablo Padilla <jesusp@parabolt.net>
2020-02-25 16:26:56 -0600

    final commit before implementing relational db mappings

--- controller/ResourcesController.java
+++ controller/ResourcesController.java
@@ -1,7 +1,9 @@
 package com.parabolt.academyHackaton.back1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,9 +18,12 @@ public class ResourcesController {
 		}
 	
 		
-		@GetMapping("/testpage")
-		public String getTestIndex() {
+		@GetMapping("/testpage/{stg}")
+		public String getTestIndex(Model model, @PathVariable Integer stg) {
 			
+			
+			
+			model.addAttribute("stage", stg);
 			return "test_index";
 		}
 }
